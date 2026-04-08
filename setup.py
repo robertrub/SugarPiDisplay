e2ea2d43fc10efcfd11e2646549159d5c552f11e
Robrub <76626474+robertrub@users.noreply.github.com>
2026-04-08 15:15:40 +0200

    Update setup.py for version and dependencies

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sugarpidisplay',
-    version='0.13',
-    description='Display your CGM data on a tiny LCD or epaper screen',
-    url='https://github.com/bassettb/SugarPiDisplay',
+    version='1.0.0',
+    description='Display your CGM data on a Waveshare e-paper display',
+    url='https://github.com/robertrub/SugarPiDisplay',
     author='Bryan Bassett',
     license='MIT',
-    packages=['sugarpidisplay'],
+    packages=find_packages(),
+    python_requires='>=3.8',
     install_requires=[
-        'Flask',
-        'Flask-WTF',
-        'Pillow',
-        'spidev',
-        'RPi.GPIO'
+        'Flask>=2.3.0',
+        'Flask-WTF>=1.1.0',
+        'Pillow>=10.0.0',
+        'spidev>=3.6',
+        'RPi.GPIO>=0.7.0',
+        'requests>=2.31.0',
     ],
     zip_safe=False
 )
